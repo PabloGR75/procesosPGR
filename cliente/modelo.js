d18ae94224d6f50b75d79412f1575dccf84b5bfb
PabloGR75 <159147691+PabloGR75@users.noreply.github.com>
2024-10-08 18:58:11 +0200

    ampliación modelo

--- cliente/modelo.js
+++ cliente/modelo.js
@@ -4,6 +4,9 @@ function Sistema(){
 
   // operaciones sobre la colección
   this.agregarUsuario=function(nick){
+    if(!nick){
+      return false;
+    }
     if(!this.usuarios[nick]){
       this.usuarios[nick] = new Usuario(nick);
     }
@@ -15,6 +18,18 @@ function Sistema(){
       delete this.usuarios[nick];
       rdo = true
     }
+  }
+
+  this.obtenerUsuarios=function(){
+    return this.usuarios;
+  }
+
+  this.usuarioActivo=function(nick){
+    return this.usuarios[nick]!=undefined;
+  }
+
+  this.numeroUsuarios=function(){
+    return Object.keys(this.usuarios).length;
   }
 }
 
